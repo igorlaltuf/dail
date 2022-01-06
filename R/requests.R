132f60d2a5963d81021e9ae4f0111bbfdee1ca93
Igor Laltuf <igorlaltuf@gmail.com>
2022-01-06 12:29:24 -0300

    remoção de comentário

--- R/requests.R
+++ R/requests.R
@@ -1,9 +1,5 @@
 # Script containing the function to query the requests made to Brazilian federal government through Right to Information Law
 # The search argument takes a string that must be found in the 'detalhamento' column
-# utils::globalVariables(c("X1", "X2", "X3","X4","X5","X6","X7","X8","X9",
-#                          "X10","X11","X12","X13","X14","X15","X16","X17",
-#                          "X18","X19","X20","X21","palavras","download.file",
-#                          "unzip","protocolo"))
 
 #' Query the requests made through Right to Information Law to Brazilian federal government
 #'
