c35c8f472e54e446751d870d0a6b0c19689894a5
Igor Laltuf <igorlaltuf@gmail.com>
2022-09-20 22:00:54 -0300

    Add: dataframe with agencies initials.

---
+++ R/agencies_initials.R
@@ -0,0 +1,13 @@
+#' Public agencies available.
+#'
+#' A dataset containing federal public agencies available to search in the agency argument from the functions requests and appeals.
+#'
+#'
+#' @format A data frame with 311 rows and 2 variables:
+#' \describe{
+#'   \item{agencies_initials}{public agency initials}
+#'   \item{agencies_names}{public agency name}
+#'
+#' }
+#' @source \url{http://www.consultaesic.cgu.gov.br/busca/_layouts/15/downloadpedidos/downloaddados.aspx}
+"agencies_initials"
